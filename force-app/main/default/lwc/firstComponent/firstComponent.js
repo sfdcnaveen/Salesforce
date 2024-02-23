efe756f73852ab90e21308f10fe592d92f9eea65
Naveen Kumar Pasupuleti <pasupulatink@gmail.com>
2024-02-23 07:44:52 +0530

    Implemented Add and Remove buttons to the component

--- force-app/main/default/lwc/firstComponent/firstComponent.js
+++ force-app/main/default/lwc/firstComponent/firstComponent.js
@@ -1,8 +1,22 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track } from "lwc";
 
 export default class FirstComponent extends LightningElement {
-    @track welcomeNote = "Hello, Good Morning!";
-    handleInputChange(event){
-        this.welcomeNote = event.target.value;
+  @track welcomeNote = "Hello, Good Morning!";
+  @track newText = "";
+  @track items = [];
+  handleInputChange(event) {
+    this.welcomeNote = event.target.value;
+    this.newText = event.target.value;
+  }
+  handleAddClick() {
+    if (this.newText) {
+      //push method adds the element into the array
+      this.items.push(this.newText);
+      this.newText = ""; // Clear the input field
     }
+  }
+  handleRemoveClick() {
+    //pop method removes the last element of the array
+    this.items.pop();
+  }
 }
