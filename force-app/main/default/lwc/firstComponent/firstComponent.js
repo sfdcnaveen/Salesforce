37bb66ae502142fad1a3afa96fbfb52fa34a6379
Naveen Kumar Pasupuleti <pasupulatink@gmail.com>
2024-03-09 18:57:18 +0530

    implemented showRed in getter

--- force-app/main/default/lwc/firstComponent/firstComponent.js
+++ force-app/main/default/lwc/firstComponent/firstComponent.js
@@ -2,15 +2,18 @@ import { LightningElement, track } from "lwc";
 
 export default class FirstComponent extends LightningElement {
   @track welcomeNote = "Hello, Good Morning!";
-  @track showRed = false;
+  //@track showRed = false;
   @track newText = "";
   @track items = [];
   @track checkboxSelected = false;
   students = ['HariTeja','Naveen','Nageswari','Lakshmi','Srinivasulu'];
+  get showRed(){
+    return this.welcomeNote === undefined || this.welcomeNote === null || this.welcomeNote === '' || !this.checkboxSelected;
+  }
   handleInputChange(event) {
     this.welcomeNote = event.target.value;
     this.newText = event.target.value;
-    this.showRed = this.welcomeNote === undefined || this.welcomeNote === null || this.welcomeNote === '' || !this.checkboxSelected;
+    //this.showRed = this.welcomeNote === undefined || this.welcomeNote === null || this.welcomeNote === '' || !this.checkboxSelected;
   }
   handleAddClick() {
     if (this.newText) {
@@ -26,6 +29,6 @@ export default class FirstComponent extends LightningElement {
   handleChange(event){
     this.checkboxSelected = event.target.checked;
     console.log(this.checkboxSelected);
-    this.showRed = this.welcomeNote === undefined || this.welcomeNote === null || this.welcomeNote === '' || !this.checkboxSelected;
+    //this.showRed = this.welcomeNote === undefined || this.welcomeNote === null || this.welcomeNote === '' || !this.checkboxSelected;
   }
 }
