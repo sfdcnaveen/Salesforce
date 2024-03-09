be3fcbd77609d03cde43b6978c641e3502d5df38
Naveen Kumar Pasupuleti <pasupulatink@gmail.com>
2024-03-09 20:23:56 +0530

    Implemented a checkbox group in child component and a input field in parent class. If the typed value in input box matches to checkbox group value then it will automatically selected

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -2,4 +2,20 @@ import { LightningElement, api } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
     @api firstLabel;
+    value = ['HariTeja'];
+    get options() {
+        return [
+            { label: 'Naveen', value: 'Naveen' },
+            { label: 'Nageswari', value: 'Nageswari' },
+            { label: 'HariTeja', value: 'HariTeja' },
+            { label: 'Srinivasulu', value: 'Srinivasulu' },
+            { label: 'Lakshmi', value: 'Lakshmi' },
+            { label: 'Prasanna', value: 'Prasanna' },
+            { label: 'SaiKrupa', value: 'SaiKrupa'}
+        ];
+    }
+    @api
+    valuesFromParent(fName) {
+        this.value = fName.split(',');
+    }
 }
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class ParentComponent extends LightningElement {}
+export default class ParentComponent extends LightningElement {
+    handleInput(event){
+        let fName = event.target.value;
+        const childComp = this.template.querySelector('c-child-component');
+        childComp.valuesFromParent(fName);
+    }
+}
