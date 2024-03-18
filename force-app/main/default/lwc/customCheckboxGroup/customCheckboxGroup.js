f8c93175007da9e55f29fac444ecec5c2278e187
Naveen Kumar Pasupuleti <pasupulatink@gmail.com>
2024-03-18 07:28:18 +0530

    Fixed Custom Checkbox Group component

--- force-app/main/default/lwc/customCheckboxGroup/customCheckboxGroup.js
+++ force-app/main/default/lwc/customCheckboxGroup/customCheckboxGroup.js
@@ -4,7 +4,7 @@ export default class CustomCheckboxGroup extends LightningElement {
     
     value = ['HariTeja'];
     options = [
-        { label: 'Naveen', value: 'Naveen' },
+        { label: 'Naveen', value: 'Naveen'},
         { label: 'Nageswari', value: 'Nageswari' },
         { label: 'HariTeja', value: 'HariTeja' },
         { label: 'Srinivasulu', value: 'Srinivasulu' },
@@ -12,8 +12,15 @@ export default class CustomCheckboxGroup extends LightningElement {
         { label: 'Prasanna', value: 'Prasanna' },
         { label: 'SaiKrupa', value: 'SaiKrupa'}
     ];
+
+    get updatedOptions(){
+        return this.options.map(x=>{
+            return {'label':x.label,'isChecked':(this.value.indexOf(x.value)>=0)};
+        })
+    }
     handleInput(event){
         let fName = event.target.value;
         this.value = fName.split(',');
+        //this.value = event.target.value;
     }
 }
